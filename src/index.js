e5ed30ad03d50966dc47923daf76edb456b0af03
Oliver Coimbra <olivercoimbra@gmail.com>
2021-12-23 22:34:56 -0400

    modify file for updates

--- src/index.js
+++ src/index.js
@@ -1,15 +1,51 @@
 // eslint-disable-next-line
 import _ from 'lodash';
 import './style.css';
-import { displayTable } from './tablero.js';
-import { addScoreBox } from './addScore.js';
+import { sendNewData, receiveData } from './dataAPI';
 
-const titleh1 = document.getElementById('title-h1');
-const titulo = document.createElement('h1');
-titulo.innerHTML = 'Leaderboard';
-titulo.classList.add('display-1');
-titleh1.appendChild(titulo);
-const contenedor = document.getElementById('container');
-contenedor.classList.add('d-flex');
-contenedor.appendChild(displayTable());
-contenedor.appendChild(addScoreBox());
+
+export const scoresContainer = document.querySelector('.scores-container');
+const nameInput = document.querySelector('.name-input');
+const scoreInput = document.querySelector('.score-input');
+const addScoreBtn = document.querySelector('.submit-btn');
+const refreshBtn = document.querySelector('.refresh-btn');
+
+export const populateContainer = (value) => {
+  value.forEach((el) => {
+    const textHtml = `<li class="specific-score">${el.user}&#58; ${el.score}</li>`;
+    scoresContainer.insertAdjacentHTML('afterbegin', textHtml);
+  });
+};
+
+const addScoreFunc = () => {
+  sendNewData(nameInput.value, scoreInput.value);
+  nameInput.value = '';
+  scoreInput.value = '';
+};
+
+window.onload = () => {
+  receiveData();
+
+  addScoreBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    addScoreFunc();
+  });
+
+  refreshBtn.addEventListener('click', () => {
+    scoresContainer.innerHTML = '';
+    receiveData();
+  });
+};
+
+// fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games', {
+//   method: 'POST',
+//   body: JSON.stringify({
+//     "name": 'My cool new game',
+//   }),
+//   headers: {
+//     'Content-type': 'application/json; charset=UTF-8',
+//   },
+// })
+//   .then((response) => response.json())
+//   .then((json) => console.log(json));
+  
