93c2f066efe19f5d929d084818e16576d6eba758
Oliver Coimbra <olivercoimbra@gmail.com>
2021-12-23 22:35:24 -0400

    modify file for updates

---
+++ src/dataAPI.js
@@ -0,0 +1,37 @@
+
+// eslint-disable-next-line import/no-cycle
+import { populateContainer } from './index';
+const displayMsg = document.querySelector('.display-msg');
+const spaceGameID = 'FRQ4BNagXuOdP7UyIiqT';
+const addressAPI = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/';
+const renderMsg = (msg) => {
+  displayMsg.textContent = msg;
+};
+
+export const sendNewData = (username, score) => {
+  fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/FRQ4BNagXuOdP7UyIiqT/scores/', {
+    method: 'POST',
+    body: JSON.stringify({
+      user: username,
+      score,
+    }),
+    headers: { 'Content-type': 'application/json; charset=UTF-8' },
+  })
+    .then((data) => {
+      if(data.ok){
+        renderMsg('Succesfully added a new record');
+      }else{
+        throw new Error(`No response: ${data.status}`);
+      }
+    })
+    .catch((err) => {
+      renderMsg('Something went wrong: ${err.message}. Try again');
+    });
+};
+
+export const receiveData = async () => {
+  const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/FRQ4BNagXuOdP7UyIiqT/scores/');
+  const data = await response.json();
+  const sortedScores = data.result.sort((a, b) => a.score - b.score);
+  populateContainer(sortedScores);
+};
